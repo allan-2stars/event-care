f1c125badfcdda313d6a4b13654a0d4dbb9269e9
Allan Wang <wang.allan@outlook.com>
2019-02-20 10:43:23 +1100

    add User model, add user related for events

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -21,11 +23,22 @@ app.use(
       date: String!
     }
 
+    type User{
+      _id:ID!
+      email: String!
+      password: String
+    }
+
     input EventInput {
       title: String!
       description: String!
       price: Float!
       date: String!
+    }
+
+    input UserInput{
+      email: String!
+      password: String!
     }
 
     type RootQuery{
@@ -34,6 +47,7 @@ app.use(
 
     type RootMutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
     }
     
     schema{
@@ -61,19 +75,59 @@ app.use(
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: new Date(args.eventInput.date)
+          date: new Date(args.eventInput.date),
+          creator: '5c6c8ed04d792b295cf1ef0a'
         });
+        let createdEvent;
         // "return" the evnet, so the resolver will await for the completion
         // as async, otherwise, will run instantly, and get invalid result.
         return event
           .save()
           .then(result => {
-            console.log(result);
+            createdEvent = { ...result._doc, _id: result.id };
             // return enriched document
-            return { ...result._doc };
+            return User.findById('5c6c8ed04d792b295cf1ef0a');
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error('User not found.');
+            }
+            // "push" is a mongoose method,
+            // input can be either whole object or the id
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
           })
           .catch(err => {
             console.log(err);
+            throw err;
+          });
+      },
+
+      createUser: args => {
+        const { email, password } = args.userInput;
+        // use return to wait for the correct result
+        return User.findOne({ email })
+          .then(user => {
+            if (user) {
+              throw new Error('User existes.');
+            }
+            return bcrypt.hash(password, 12);
+          })
+
+          .then(hashedPassword => {
+            const user = new User({
+              email: email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch(err => {
             throw err;
           });
       }
--- models/event.js
+++ models/event.js
@@ -21,6 +21,10 @@ const eventSchema = new Schema({
   date: {
     type: Date,
     required: true
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
   }
 });
 
