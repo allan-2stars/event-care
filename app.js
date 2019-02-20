97dc62f229add526752d51a40b71884226c079fd
Allan Wang <wang.allan@outlook.com>
2019-02-20 13:17:09 +1100

    code split into mutiple files for resolver and shema

--- app.js
+++ app.js
@@ -1,137 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
-const Event = require('./models/event');
-const User = require('./models/user');
-
+const graphQlSchema = require('./graphql/schema');
+const graphQlResolvers = require('./graphql/resolvers');
 const app = express();
 
 app.use(bodyParser.json());
+
 app.use(
   '/graphql',
   graphqlHttp({
-    schema: buildSchema(`
-    type Event {
-      _id: ID!
-      title: String!
-      description: String!
-      price: Float!
-      date: String!
-    }
-
-    type User{
-      _id:ID!
-      email: String!
-      password: String
-    }
-
-    input EventInput {
-      title: String!
-      description: String!
-      price: Float!
-      date: String!
-    }
-
-    input UserInput{
-      email: String!
-      password: String!
-    }
-
-    type RootQuery{
-      events: [Event!]!
-    }
-
-    type RootMutation {
-      createEvent(eventInput: EventInput): Event
-      createUser(userInput: UserInput): User
-    }
-    
-    schema{
-            query:RootQuery
-            mutation:RootMutation
-        }
-    `),
-    rootValue: {
-      // must be same name as type RootQuery name events
-      events: () => {
-        return Event.find()
-          .then(events => {
-            return events.map(event => {
-              //return { ...event._doc, _id: event._doc._id.toString() };
-              // mongoose has a special
-              return { ...event._doc, _id: event._id };
-            });
-          })
-          .catch(err => {
-            throw err;
-          });
-      },
-      createEvent: args => {
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date),
-          creator: '5c6c8ed04d792b295cf1ef0a'
-        });
-        let createdEvent;
-        // "return" the evnet, so the resolver will await for the completion
-        // as async, otherwise, will run instantly, and get invalid result.
-        return event
-          .save()
-          .then(result => {
-            createdEvent = { ...result._doc, _id: result.id };
-            // return enriched document
-            return User.findById('5c6c8ed04d792b295cf1ef0a');
-          })
-          .then(user => {
-            if (!user) {
-              throw new Error('User not found.');
-            }
-            // "push" is a mongoose method,
-            // input can be either whole object or the id
-            user.createdEvents.push(event);
-            return user.save();
-          })
-          .then(result => {
-            return createdEvent;
-          })
-          .catch(err => {
-            console.log(err);
-            throw err;
-          });
-      },
-
-      createUser: args => {
-        const { email, password } = args.userInput;
-        // use return to wait for the correct result
-        return User.findOne({ email })
-          .then(user => {
-            if (user) {
-              throw new Error('User existes.');
-            }
-            return bcrypt.hash(password, 12);
-          })
-
-          .then(hashedPassword => {
-            const user = new User({
-              email: email,
-              password: hashedPassword
-            });
-            return user.save();
-          })
-          .then(result => {
-            return { ...result._doc, password: null, _id: result.id };
-          })
-          .catch(err => {
-            throw err;
-          });
-      }
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
 
     // set this true, you have the graphql tool in browser
     graphiql: true
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,142 @@
+const bcrypt = require('bcrypt');
+
+const Event = require('../../models/event');
+const User = require('../../models/user');
+// find all the event for every event Id
+// eventIds is an array of Event ID
+// below function, use async await method for fun.
+// use try catch
+const events = async eventIds => {
+  // no need use 'return'
+  // top most always returned.
+  const events = await Event.find({ _id: { $in: eventIds } });
+  try {
+    //.then(events => {
+    return events.map(event => {
+      return {
+        ...event._doc,
+        _id: event.id,
+        date: readableDate(event._doc.date),
+        // use function to drill deeper,
+        // drill down to get rich data of the user for this event
+        creator: user.bind(this, event.creator)
+      };
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+// helper function to get the user id as can be used in GraphQL
+const user = userId => {
+  return User.findById(userId)
+    .then(user => {
+      return {
+        ...user._doc,
+        _id: user.id,
+        // use function to drill deeper,
+        // drill down to get rich data of the event for this user
+        createdEvents: events.bind(this, user._doc.createdEvents)
+      };
+    })
+    .catch(err => {
+      throw err;
+    });
+};
+
+// convert Date to readable format
+const readableDate = date => {
+  return new Date(date).toISOString();
+};
+
+module.exports = {
+  // must be same name as type RootQuery name events
+  events: () => {
+    return Event.find()
+      .populate('creator')
+      .then(events => {
+        return events.map(event => {
+          //return { ...event._doc, _id: event._doc._id.toString() };
+          // mongoose has a special
+          return {
+            ...event._doc,
+            _id: event._id,
+            date: readableDate(event._doc.date),
+            // convert database object ID to string ID
+            creator: user.bind(this, event._doc.creator)
+          };
+        });
+      })
+      .catch(err => {
+        throw err;
+      });
+  },
+  createEvent: args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: '5c6c8ed04d792b295cf1ef0a'
+    });
+    let createdEvent;
+    // "return" the evnet, so the resolver will await for the completion
+    // as async, otherwise, will run instantly, and get invalid result.
+    return event
+      .save()
+      .then(result => {
+        createdEvent = {
+          ...result._doc,
+          _id: result.id,
+          date: readableDate(event._doc.date),
+          // date: event._doc.date,
+          // enrich the creator for deeper data
+          creator: user.bind(this, result._doc.creator)
+        };
+        // return enriched document
+        return User.findById('5c6c8ed04d792b295cf1ef0a');
+      })
+      .then(user => {
+        if (!user) {
+          throw new Error('User not found.');
+        }
+        // "push" is a mongoose method,
+        // input can be either whole object or the id
+        user.createdEvents.push(event);
+        return user.save();
+      })
+      .then(result => {
+        return createdEvent;
+      })
+      .catch(err => {
+        console.log(err);
+        throw err;
+      });
+  },
+
+  createUser: args => {
+    const { email, password } = args.userInput;
+    // use return to wait for the correct result
+    return User.findOne({ email })
+      .then(user => {
+        if (user) {
+          throw new Error('User existes.');
+        }
+        return bcrypt.hash(password, 12);
+      })
+
+      .then(hashedPassword => {
+        const user = new User({
+          email: email,
+          password: hashedPassword
+        });
+        return user.save();
+      })
+      .then(result => {
+        return { ...result._doc, password: null, _id: result.id };
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
+};
