3ccc2f741181d0bb3303f4f8eb61c76651a89630
Allan Wang <wang.allan@outlook.com>
2019-02-22 14:50:43 +1100

    Add Navigation and some Styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
@@ -11,14 +12,20 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        {/* with Switch, only search the first match, and do not check the rest */}
-        <Switch>
-          {/* with Redirect, redirect from some path to some else path */}
-          <Redirect from='/' to='/auth' exact />
-          <Route path='/auth' exact component={AuthPage} />
-          <Route path='/events' exact component={EventsPage} />
-          <Route path='/bookings' exact component={BookingsPage} />
-        </Switch>
+        {/* // React.Fragment is a shell HTML element for wrap around. */}
+        <React.Fragment>
+          <MainNavigation />
+          <main className='main-content'>
+            {/* with Switch, only search the first match, and do not check the rest */}
+            <Switch>
+              {/* with Redirect, redirect from some path to some else path */}
+              <Redirect from='/' to='/auth' exact />
+              <Route path='/auth' exact component={AuthPage} />
+              <Route path='/events' exact component={EventsPage} />
+              <Route path='/bookings' exact component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
