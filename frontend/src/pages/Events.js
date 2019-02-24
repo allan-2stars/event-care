b418616a9a92bfe8f92fa6b9c46895c1aa0f80af
Allan Wang <wang.allan@outlook.com>
2019-02-24 21:46:54 +1100

    Add Event and send to backend. just console log the res data.

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -2,20 +2,87 @@ import React, { Component } from 'react';
 
 import Modal from '../components/Modal/Modal';
 import Backdrop from '../components/Backdrop/Backdrop';
+import AuthContext from '../context/auth-context';
 import './Event.css';
+import authContext from '../context/auth-context';
+import { Collection } from 'mongoose';
 
 class EventsPage extends Component {
   state = {
     creating: false
   };
 
+  static contextType = authContext;
+
+  constructor(props) {
+    super(props);
+    this.titleEl = React.createRef();
+    this.priceEl = React.createRef();
+    this.dateEl = React.createRef();
+    this.descriptionEl = React.createRef();
+  }
+
   startCreateEventHandler = () => {
     this.setState({ creating: true });
   };
 
   modalConfirmHandler = () => {
-    console.log('confirm');
     this.setState({ creating: false });
+
+    const title = this.titleEl.current.value;
+    const price = +this.priceEl.current.value;
+    const date = this.dateEl.current.value;
+    const description = this.descriptionEl.current.value;
+    if (
+      title.trim().length === 0 ||
+      !price ||
+      price <= 0 ||
+      date.trim().length === 0 ||
+      description.trim().length === 0
+    ) {
+      return;
+    }
+    const event = { title, price, date, description };
+
+    const requestBody = {
+      query: `mutation {
+          createEvent(eventInput:{title: "${title}", price: ${price}, date: "${date}", description: "${description}"}){
+            _id
+            title
+            date
+            description
+            price
+            creator{
+              _id
+              email
+            }
+          }
+        }`
+    };
+
+    // get the authorization token first
+    const token = this.context.token;
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log('result', resData);
+      })
+      .catch(err => {
+        console.log('Errors Happening:', err);
+      });
   };
   modalCancelHandler = () => {
     this.setState({ creating: false });
@@ -34,16 +101,34 @@ class EventsPage extends Component {
             onConfirm={this.modalConfirmHandler}
           >
             <p>Modal Content</p>
-            <form />
+            <form>
+              <div className='form-control'>
+                <label htmlFor='title'>Title</label>
+                <input type='text' id='title' ref={this.titleEl} />
+              </div>
+              <div className='form-control'>
+                <label htmlFor='price'>Price</label>
+                <input type='number' id='price' ref={this.priceEl} />
+              </div>
+              <div className='form-control'>
+                <label htmlFor='date'>Date</label>
+                <input type='datetime-local' id='date' ref={this.dateEl} />
+              </div>
+              <div className='form-control'>
+                <label htmlFor='description'>Description</label>
+                <textarea id='description' row='4' ref={this.descriptionEl} />
+              </div>
+            </form>
           </Modal>
         )}
-
-        <div className='events-control'>
-          <p>Share your own Events!</p>
-          <button className='btn' onClick={this.startCreateEventHandler}>
-            Create Event
-          </button>
-        </div>
+        {this.context.token && (
+          <div className='events-control'>
+            <p>Share your own Events!</p>
+            <button className='btn' onClick={this.startCreateEventHandler}>
+              Create Event
+            </button>
+          </div>
+        )}
       </React.Fragment>
     );
   }
